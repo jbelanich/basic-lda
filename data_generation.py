6da717148e5a95810a106205ccbf927b2549176a
Josh Belanich <jbelanich@gmail.com>
2014-06-02 18:06:10 -0400

    Fixed a couple of bugs. Also dramatically sped up algorithm

--- data_generation.py
+++ data_generation.py
@@ -1,4 +1,5 @@
 from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
 
 def basicCorpus():
 	texts = loadTexts()
@@ -19,14 +20,45 @@ def loadTexts():
 
 	return corpus
 
-def getWordsForTopic(corpus,assignments,vectorizer):
+def getWordsForTopic(corpus,assignments,vectorizer, topic):
 	features = vectorizer.get_feature_names()
 
 	rows,cols = corpus.nonzero()
 
-	for row in rows:
+	for row in set(rows):
 		print "Corpus ", row
 		for col in cols:
-			print features[col], ": ", assignments[row,col]
+			if assignments[row,col] == topic:
+				print features[col], ": ", assignments[row,col]
 
 		print " ===================== "
+
+def dailyKosBagOfWords():
+	"""
+		Translate from kos data to data my code understands.
+		This is the worst thing ever. I should be punished.
+	"""
+	vocab = []
+	with open('./dailykosvocab.txt', 'r') as vocab_handle:
+		for line in vocab_handle:
+			vocab.append(line)
+
+	wordCounts = np.loadtxt('./docword.kos.txt')
+
+	docs = []
+	lastDoc = wordCounts[0,0]
+	docWords = []
+	for i in xrange(wordCounts.shape[0]):
+		doc = wordCounts[i,0]
+		wordIndex = wordCounts[i,1]
+		wordCount = wordCounts[i,2]
+
+		if doc != lastDoc:
+			docs.append(' '.join(docWords))
+			docWords = []
+			lastDoc = doc
+
+		for j in xrange(int(wordCount)):
+			docWords.append(vocab[int(wordIndex)-1])
+
+	return docs
--- lda.py
+++ lda.py
@@ -2,12 +2,9 @@ import numpy as n
 import scipy.sparse as sparse
 from scipy import *
 
-numTopics = 10
-
-
 class LDAModel:
 
-	def __init__(self, corpus, alpha=None, beta=None):
+	def __init__(self, corpus, numTopics = 10, alpha=None, beta=None):
 		"""
 			Initializes a LDAModel based on the given corpus. Alpha and
 			Beta represent the hyperparameters of the model. If none are
@@ -32,6 +29,7 @@ class LDAModel:
 
 		self.__vocabCounts = sparse.lil_matrix((self.vocabSize,self.numTopics))
 		self.__documentCounts = sparse.lil_matrix((self.numDocuments, self.numTopics))
+		self.__vocabMarginals = n.zeros([self.numTopics])
 
 		self.initialize()
 
@@ -48,6 +46,8 @@ class LDAModel:
 			self.__documentCounts[row, assignment] += self.__corpus[row,col]
 			self.__vocabCounts[col, assignment] += self.__corpus[row,col]
 
+		self.calculateVocabMarginals()
+
 	def gibbs(self, iterations):
 		"""
 			Runs gibbs sampling step for the provided number of iterations.
@@ -62,7 +62,6 @@ class LDAModel:
 		"""
 			Refresh all topic assignments.
 		"""
-
 		rows, cols = self.__corpus.nonzero()
 		for row, col in zip(rows,cols):
 			#construct probability dist over topic assignments
@@ -71,6 +70,15 @@ class LDAModel:
 			newAssignment = n.random.choice(self.numTopics, p=dist)
 			if newAssignment != self.__assignments[row,col]:
 				self.updateAssignment(row,col,newAssignment)
+
+	def calculateVocabMarginals(self):
+		for k in xrange(self.numTopics):
+			counts = []
+			rows, cols = self.__vocabCounts[:,k].nonzero()
+			for r in set(rows):
+				counts.append(self.__vocabCounts[r,k] + self.__beta[r])
+
+			self.__vocabMarginals[k] = sum(counts)
 
 	def updateAssignment(self, doc, word, newTopic):
 		"""
@@ -81,10 +89,9 @@ class LDAModel:
 		self.__documentCounts[doc,newTopic] += self.__corpus[doc,word]
 		self.__vocabCounts[word,oldTopic] -= self.__corpus[doc,word]
 		self.__vocabCounts[word,newTopic] += self.__corpus[doc,word]
+		self.__vocabMarginals[newTopic] += self.__corpus[doc,word]
+		self.__vocabMarginals[oldTopic] -= self.__corpus[doc,word]
 		self.__assignments[doc,word] = newTopic
-
-		assert (self.__vocabCounts[word,oldTopic] >= 0)
-		assert (self.__documentCounts[doc,oldTopic] >= 0)
 
 	def getExcludedVocabCount(self, word, topic, exclude):
 		(m,n) = exclude
@@ -94,13 +101,6 @@ class LDAModel:
 			toSubtract = self.__corpus[m,n]
 		else:
 			toSubtract = 0
-
-		if (self.__vocabCounts[word,topic] - toSubtract) < 0:
-			print "Vocab Counts Problem: "
-			print self.__vocabCounts[word,topic]
-			print toSubtract
-
-		assert (self.__vocabCounts[word,topic] - toSubtract >= 0)
 
 		return self.__vocabCounts[word,topic] - toSubtract
 
@@ -112,31 +112,26 @@ class LDAModel:
 		else:
 			toSubtract = 0
 
-		if (self.__documentCounts[document,topic] - toSubtract) < 0:
-			print "Document Counts Problem: "
-			print self.__documentCounts[document,topic]
-			print toSubtract
-
-		assert (self.__documentCounts[document,topic] - toSubtract >= 0)
-
 		return self.__documentCounts[document,topic] - toSubtract
 
-	def getVocabNorm(self, topic, exclude):
-		counts = []
-		rows, cols = self.__vocabCounts[:,topic].nonzero()
-		for r in rows:
-			counts.append(self.getExcludedVocabCount(r, topic, exclude) + self.__beta[r])
+	def getExcludedVocabNorm(self, topic, exclude):
+		(m,n) = exclude
 
-		return sum(counts)
+		if self.__assignments[m,n] == topic:
+			toSubtract = self.__corpus[m,n]
+		else:
+			toSubtract = 0
+
+		return self.__vocabMarginals[topic] - toSubtract
 
 
 	def topicDistribution(self, document, word):
 		#first calculate vocabulary normalization
 		dist = []
 		for i in xrange(self.numTopics):
-			prob = self.getExcludedDocumentCount(document,i,(document,word)) + self.__alpha[i]
+			prob = self.getExcludedDocumentCount(document, i, (document,word)) + self.__alpha[i]
 			vocabProb = self.getExcludedVocabCount(word, i, (document,word)) + self.__beta[word]
-			vocabNorm = self.getVocabNorm(i, (document, word))
+			vocabNorm = self.getExcludedVocabNorm(i, (document,word))
 			dist.append(prob * (vocabProb/vocabNorm))
 
 		return dist/sum(dist)
