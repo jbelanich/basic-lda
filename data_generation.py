d97ca7ac9d058fea202b02f987a6f0c54bbf88aa
Josh Belanich <jbelanich@gmail.com>
2014-06-09 02:43:10 -0400

    Refactored data_generation.py

--- data_generation.py
+++ data_generation.py
@@ -1,132 +1,40 @@
-from sklearn.feature_extraction.text import CountVectorizer
-import numpy as np
 from corpus import *
 
-def basicCorpus():
-	texts = loadTexts()
-	vectorizer = CountVectorizer(min_df = 1)
-	features = vectorizer.fit_transform(texts)
-	return corpusFromVectorizer(features, vectorizer)
-
-def loadTexts():
-	toLoad = ['./dogs.txt', './cats.txt', 'pets.txt']
-	corpus = []
-
-	for f in toLoad:
-		s = []
-		with open(f, 'r') as f_handle:
-			for line in f_handle:
-				s.append(line)
-
-		corpus.append(''.join(s))
-
-	return corpus
-
-def getWordsForTopic(corpus,assignments,vectorizer, topic):
-	features = vectorizer.get_feature_names()
-
-	rows,cols = corpus.nonzero()
-
-	for row in set(rows):
-		print "Docuent ", row
-		for col in cols:
-			if assignments[row,col] == topic:
-				print features[col], ": ", assignments[row,col]
-
-		print " ===================== "
-
-def dailyKosBagOfWords():
-	"""
-		Translate from kos data to data my code understands.
-		This is the worst thing ever. I should be punished.
-	"""
-	vocab = []
-	with open('./dailykosvocab.txt', 'r') as vocab_handle:
-		for line in vocab_handle:
-			vocab.append(line)
-
-	wordCounts = np.loadtxt('./docword.kos.txt')
-
-	docs = []
-	lastDoc = wordCounts[0,0]
-	docWords = []
-	for i in xrange(wordCounts.shape[0]):
-		doc = wordCounts[i,0]
-		wordIndex = wordCounts[i,1]
-		wordCount = wordCounts[i,2]
-
-		if doc != lastDoc:
-			docs.append(' '.join(docWords))
-			docWords = []
-			lastDoc = doc
-
-		for j in xrange(int(wordCount)):
-			docWords.append(vocab[int(wordIndex)-1])
-
-	return docs
+def nipsCountMatrix(numDocs=None):
+	nipsVocab = 'data/nipsvocab.txt'
+	nipsCounts = 'data/docword.nips.txt'
+	return filesToCorpus(nipsVocab,nipsCounts,numDocs)
 
 def dailyKosCountMatrix(numDocs=None):
-	vocab = []
-	with open('./dailykosvocab.txt', 'r') as vocab_handle:
-		for line in vocab_handle:
-			vocab.append(line)
-
-	wordCounts = np.loadtxt('./docword.kos.txt')
-
-	docs = []
-	lastDoc = wordCounts[0,0]
-	docWords = {}
-	for i in xrange(wordCounts.shape[0]):
-		doc = int(wordCounts[i,0])
-		wordIndex = int(wordCounts[i,1])
-		wordCount = int(wordCounts[i,2])
-
-		if doc != lastDoc:
-			docs.append(docWords)
-			docWords = {}
-			lastDoc = doc
-
-			if numDocs and doc > numDocs:
-				break
-
-		docWords[wordIndex-1] = wordCount
-
-	return Corpus(docs, vocab)
+	kosVocab = 'data/dailykosvocab.txt'
+	kosCounts = 'data/docword.kos.txt'
+	return filesToCorpus(kosVocab,kosCounts,numDocs)
 
 def filesToCorpus(vocabFile,countFile, numDocs=None):
+	
 	vocab = []
 	with open(vocabFile, 'r') as vocab_handle:
 		for line in vocab_handle:
 			vocab.append(line)
 
-	wordCounts = np.loadtxt(countFile)
+	
+	with open(countFile, 'r') as count_handle:
+		#First three numbers are D, V, and N
+		[numDocs,numWords,totalCounts] = [int(count_handle.readline()) \
+			for _ in xrange(3)]
 
-	docs = []
-	lastDoc = wordCounts[0,0]
-	docWords = {}
-	for i in xrange(wordCounts.shape[0]):
-		doc = int(wordCounts[i,0])
-		wordIndex = int(wordCounts[i,1])
-		wordCount = int(wordCounts[i,2])
+		countMatrix = CountMatrix(numRows=numDocs)
+		for line in count_handle:
+			[doc,word,count] = [int(x) for x in line.split()]
 
-		if doc != lastDoc:
-			docs.append(docWords)
-			docWords = {}
-			lastDoc = doc
+			#docs are not zero indexed in files
+			doc -= 1
+			#neither are words
+			word -= 1
 
-			if numDocs and doc > numDocs:
+			if numDocs and doc >= numDocs:
 				break
 
-		docWords[wordIndex-1] = wordCount
-
-	return Corpus(docs, vocab)
-
-def corpusFromVectorizer(wordCounts, vectorizer):
-	vocab = vectorizer.get_feature_names()
-
-	countMatrix = CountMatrix(numRows=wordCounts.shape[0])
-	rows, cols = wordCounts.nonzero()
-	for row,col in zip(rows,cols):
-		countMatrix[row,col] = int(wordCounts[row,col])
+			countMatrix[doc,word] = count
 
 	return countMatrix.toCorpus(vocab)
